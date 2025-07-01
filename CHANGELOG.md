6a185aa222fcd7b014b921d6d107ecd2ab1e4a42
Pjotr van der Horst <pjotrvdh@hotmail.com>
2025-07-01 11:50:43 +0200

    feat: implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `swisnl/filament-geometry` will be documented in this file.
 
-## 1.0.0 - 202X-XX-XX
+## 0.1.0 - 2025-07-01
 
-- initial release
+- Initial release
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
     plugins: {
         "postcss-import": {},
         "tailwindcss/nesting": {},
+        'postcss-url': {
+            url: 'inline'
+        },
+        'postcss-assets': {},
         tailwindcss: {},
         autoprefixer: {},
     },
