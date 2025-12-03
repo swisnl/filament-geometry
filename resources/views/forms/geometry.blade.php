cc3285551c0c3a37bb4b919b0bf8b748c88d6633
Jasper Zonneveld <jasper@swis.nl>
2025-12-03 08:46:14 +0100

    fix: make sure the input gets a red border on error

--- resources/views/forms/geometry.blade.php
+++ resources/views/forms/geometry.blade.php
@@ -1,25 +1,25 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    <div
-        x-ignore
-        x-load
-        x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-geometry-styles', 'swisnl/filament-geometry'))]"
-        x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-geometry-scripts', 'swisnl/filament-geometry') }}"
-        x-data="filamentGeometry($wire, $watch, {{ $getMapConfig() }})"
-        wire:ignore
-        x-intersect.once="create($refs.map)"
-    >
-        <x-filament::input.wrapper class="overflow-hidden">
+    <x-filament::input.wrapper :valid="!$errors->has($getStatePath())" class="overflow-hidden">
+        <div
+            x-ignore
+            x-load
+            x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-geometry-styles', 'swisnl/filament-geometry'))]"
+            x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-geometry-scripts', 'swisnl/filament-geometry') }}"
+            x-data="filamentGeometry($wire, $watch, {{ $getMapConfig() }})"
+            wire:ignore
+            x-intersect.once="create($refs.map)"
+        >
             <div x-ref="map" class="h-[40dvh] z-0"></div>
-        </x-filament::input.wrapper>
-        <input
-            {{
-                $attributes
-                    ->merge([
-                        'id' => $getId(),
-                        'type' => 'hidden',
-                        $applyStateBindingModifiers('wire:model') => $getStatePath(),
-                    ], escape: FALSE)
-            }}
-        />
-    </div>
+        </div>
+    </x-filament::input.wrapper>
+    <input
+        {{
+            $attributes
+                ->merge([
+                    'id' => $getId(),
+                    'type' => 'hidden',
+                    $applyStateBindingModifiers('wire:model') => $getStatePath(),
+                ], escape: FALSE)
+        }}
+    />
 </x-dynamic-component>
